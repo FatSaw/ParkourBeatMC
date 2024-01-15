82ca9aeaed69500e6c148a892402974f6889b526
FatSaw <eklesosek@gmail.com>
2024-01-15 23:55:39 +0200

    Create LocationInside.java

---
+++ src/me/bomb/parkourbeat/LocationInside.java
@@ -0,0 +1,10 @@
+package me.bomb.parkourbeat;
+
+final class LocationInside {
+	public final double x, y, z;
+	protected LocationInside(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
