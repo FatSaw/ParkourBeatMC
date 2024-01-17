8d420d61514f06e248aec22f2606e8fbe9b38a82
FatSaw <eklesosek@gmail.com>
2024-01-17 05:43:20 +0200

    Update VoidGenerator.java

--- src/me/bomb/parkourbeat/VoidGenerator.java
+++ src/me/bomb/parkourbeat/VoidGenerator.java
@@ -5,7 +5,10 @@ import java.util.Random;
 import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
 
-public class VoidGenerator extends ChunkGenerator {
+public final class VoidGenerator extends ChunkGenerator {
+	
+	protected VoidGenerator() {
+	}
    
     public byte[][] generateBlockSections(World world, Random random, int chunkX, int chunkZ, BiomeGrid biomeGrid){
         return new byte[16][];
