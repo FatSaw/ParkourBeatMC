cb5b26edc4d612e2b651b375fd1a81167a1cda8f
FatSaw <eklesosek@gmail.com>
2024-01-17 05:43:17 +0200

    Update TptoworldTabComplete.java

--- src/me/bomb/parkourbeat/TptoworldTabComplete.java
+++ src/me/bomb/parkourbeat/TptoworldTabComplete.java
@@ -8,13 +8,16 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-public class TptoworldTabComplete implements TabCompleter {
+public final class TptoworldTabComplete implements TabCompleter {
+	
+	protected TptoworldTabComplete() {
+	}
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
 		ArrayList<String> tabcomplete = new ArrayList<String>();
 		if (sender instanceof Player&&args.length == 1) {
-			for(String ingameworld : Game.getWorlds()) {
+			for(String ingameworld : GameStarter.getWorlds()) {
 				if(ingameworld.toLowerCase().startsWith(args[0].toLowerCase())) {
 					tabcomplete.add(ingameworld);
 				}
