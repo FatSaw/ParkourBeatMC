bd53f68e9d2c9bf62772d7899a1e2a62c9b7900c
FatSaw <eklesosek@gmail.com>
2024-01-22 18:11:58 +0200

    Update EventListener.java

--- src/me/bomb/parkourbeat/EventListener.java
+++ src/me/bomb/parkourbeat/EventListener.java
@@ -1,6 +1,7 @@
 package me.bomb.parkourbeat;
 
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -8,14 +9,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.util.Vector;
 import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
 public final class EventListener implements Listener {
 
+	private static Location exitlocation = GameOptions.exitlocation.clone().add(0, 325, 0);
+	
 	@EventHandler
 	public void onSpawnLocation(PlayerSpawnLocationEvent event) {
 		event.setSpawnLocation(GameOptions.exitlocation);
@@ -36,17 +41,17 @@ public final class EventListener implements Listener {
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		Player player = event.getPlayer();
 		player.setGameMode(GameMode.ADVENTURE);
-		player.setHealth(player.getMaxHealth());
+		player.setHealth(20);
 		player.setFoodLevel(20);
 		player.setSaturation(5.0F);
 		player.setExhaustion(0.0F);
+		player.setFireTicks(-40);
 		GameStarter.leaveGame(player);
 	}
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		player.teleport(GameOptions.exitlocation);
-		player.setMaxHealth(20);
 		player.setHealth(20);
 		player.setFoodLevel(20);
 		player.setSaturation(5.0F);
@@ -63,7 +68,14 @@ public final class EventListener implements Listener {
 		Player player = (Player) entity;
 		DamageCause cause = event.getCause();
 		if(cause == DamageCause.VOID && player.getWorld() == GameOptions.lobbyworld) {
-			player.teleport(GameOptions.exitlocation);
+			Vector velocity = player.getVelocity();
+			player.teleport(exitlocation.clone().setDirection(player.getLocation().getDirection()));
+			player.setVelocity(velocity);
 		}
 	}
+	@EventHandler
+	public void onFoodLevelChange(FoodLevelChangeEvent event) {
+		event.setFoodLevel(20);
+	}
+	
 }
